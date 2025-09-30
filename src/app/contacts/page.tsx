3433a3d39760d04d651ef42aa8132b64096ab6c4
Ritik-kumar045 <ritikkumarsingh647@gmai.com>
2025-09-30 23:00:37 +0530

    edfinal

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,27 +1,27 @@
 "use client";
 
 import React, { useState } from "react";
-
 import { Meteors } from "@/components/ui/meteors";
 
-export default function page() {
+export default function Page() {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
     message: "",
   });
 
-  const handleChange = (e) => {
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
     setFormData({
       ...formData,
       [e.target.name]: e.target.value,
     });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log("Form submitted:", formData);
-    // Here, you can send formData to a backend or API
     alert("Thank you for contacting us!");
     setFormData({
       name: "",
@@ -34,7 +34,6 @@ export default function page() {
     <div className="relative w-full max-w-xl justify-center mt-40 ml-84">
       <div className="absolute inset-0 h-full w-full scale-[0.80] transform rounded-full bg-red-500 bg-gradient-to-r from-blue-500 to-teal-500 blur-3xl" />
       <div className="relative flex h-full flex-col items-start justify-end overflow-hidden rounded-2xl border border-gray-800 bg-gray-900 px-4 py-6 shadow-xl">
-      
         <h1 className="relative z-50 mb-4 text-xl font-bold text-white">
           Meteors because they&apos;re cool
         </h1>
@@ -93,8 +92,7 @@ export default function page() {
           I don&apos;t know what to write so I&apos;ll just paste something cool
           here. One more sentence because lorem ipsum is just unacceptable.
           Won&apos;t ChatGPT the shit out of this.
-        </p> 
-        {/* Meaty part - Meteor effect */}
+        </p>
         <Meteors number={20} />
       </div>
     </div>
